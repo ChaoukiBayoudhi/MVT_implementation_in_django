fdbd2e1b6003a432247292ec4aa97719d38fb294
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2024-04-03 18:11:06 +0100

    Implement models.py

--- product_app/models.py
+++ product_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Product model : Class that inherits from models.Model
+class Provider(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=15)
+    website=models.URLField(null=True,blank=True)
+    class Meta:
+        db_table='providers'
 
 class Product(models.Model):
     #define a string Field (equivalent to varchar(100) in SQL)
@@ -21,12 +28,18 @@ class Product(models.Model):
     #this Field need the library Pillow, to be installed with the command : pip install pillow
     photo=models.ImageField(upload_to='images/products',null=True,blank=True)
     #relationship between Product and provider (1-n)
+    provider=models.ForeignKey(Provider,on_delete=models.SET_NULL,null=True,blank=True)
+    class Meta:
+        db_table='products'
     
-class Provider(models.Model):
-    name=models.CharField(max_length=100)
-    email=models.EmailField(unique=True)
-    phone=models.CharField(max_length=15)
-    website=models.URLField(null=True,blank=True)
+class Address(models.Model):
+    houne_number=models.CharField(max_length=10)
+    street=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    country=models.CharField(max_length=100)
+    zip_code=models.CharField(max_length=10)
+    class Meta:
+        db_table='addresses'
     
 class Client(models.Model):
     first_name=models.CharField(max_length=100)
@@ -36,12 +49,23 @@ class Client(models.Model):
     phone_number=models.CharField(max_length=15)
     photo=models.ImageField(upload_to='images/clients',null=True,blank=True)
     #define the relationship between Client and Address (1-1)
-    
+    address=models.OneToOneField(Address,on_delete=models.CASCADE)
+    #define the relationship between Product and Client (n-n) through the model Command
+    client_products=models.ManyToManyField(Product,through='Command',through_fields=('client','product'))
+    class Meta:
+        db_table='clients'
 
-class Address(models.Model):
-    houne_number=models.CharField(max_length=10)
-    street=models.CharField(max_length=100)
-    city=models.CharField(max_length=100)
-    country=models.CharField(max_length=100)
-    zip_code=models.CharField(max_length=10)
+class Command(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    client=models.ForeignKey(Client,on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+    quantity=models.PositiveSmallIntegerField(default=1)
+    
+    class Meta:
+        db_table='commands'
+        ordering=['-date'] #descending order of date
+        #ordering=['date']
+        unique_together=['product','client']
+
+
     
